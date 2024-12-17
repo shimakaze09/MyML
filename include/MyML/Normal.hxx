1aad4bf30fa2f7a8aaf74159dc35064a26226ef9
John <yangtianzhuo970425@gmail.com>
2024-12-17 17:54:53 +1300

    Bump project version to 0.3.4 and implement to_sphere_texcoord and to_sphere_tangent methods in normal struct

--- include/MyML/Normal.hxx
+++ include/MyML/Normal.hxx
@@ -4,16 +4,28 @@
 
 #pragma once
 
-#include "Interfaces/IArray/IArray1D_Util.hxx"
+#include "Point.hxx"
+
 #include "Interfaces/IArray/ICross.hxx"
 
 namespace My {
+// it's necessary to distinguish vector and normal(bivector) when transformings
+// ref
+// - PBRT p. 86-87
+// - wikipedia-bivector: https://en.wikipedia.org/wiki/Bivector
+// - https://stackoverflow.com/questions/30465573/transforming-surface-normal-vectors-and-tangent-vectors
 template <typename T>
 struct normal : SIIT_CRTP<TemplateList<IArray1D_Util, ICross>, normal<T>,
                           TypeList<TypeList<T, Size<3>>, T>> {
   using SIIT_CRTP<TemplateList<IArray1D_Util, ICross>, normal<T>,
                   TypeList<TypeList<T, Size<3>>, T>>::SIIT_CRTP;
+
+  const point<T, 2> to_sphere_texcoord() const noexcept;
+
+  const vec<T, 3> to_sphere_tangent() const noexcept;
 };
 
 using normalf = normal<float>;
 }  // namespace My
+
+#include "detail/Normal.inl"
